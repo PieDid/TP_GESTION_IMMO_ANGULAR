f75f101ee867218d87ed6635477e80775a7be465
Cheng Jun <d.cheng@live.fr>
2020-05-12 11:15:39 +0200

    MaJ des modèles

--- angularTPImmo/src/app/modele/ICommerciaux.ts
+++ angularTPImmo/src/app/modele/ICommerciaux.ts
@@ -4,8 +4,7 @@ import { IBien } from './IBien';
  * Modèle de données pour Commerciaux
  */
 export interface ICommerciaux extends IBien {
-
-    nbPieces : number;
+    
     superficie : number;
 
 }
