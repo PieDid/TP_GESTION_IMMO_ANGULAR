edafba54ab481b79945b66eae67d20b598fa0455
andre fernandes <andre.fernandes@edu.devinci.fr>
2020-05-15 14:47:42 +0200

    page personne

--- angularTPImmo/src/app/composants/Personnes/personne-composants/create-personne/create-personne.component.ts
+++ angularTPImmo/src/app/composants/Personnes/personne-composants/create-personne/create-personne.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+/* import {MatFormFieldModule} from '@angular/material/form-field';
+import {MaterialModule} from 'src/app/material/material.module'; */
+import {FormControl, Validators} from '@angular/forms';
+/* import {MatSelect} from '@angular/material/select';
+import {MatButton, MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'; */
+import { IPersonne } from '../../../../modele/IPersonne';
+import { AdressePersonneService } from '../../../../services/adresse-personne.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { PersonneService } from '../../../../services/personne.service';
 
 @Component({
   selector: 'app-create-personne',
@@ -7,9 +17,107 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePersonneComponent implements OnInit {
 
-  constructor() { }
+
+  personne : IPersonne = {identifiant:null, nom:null, email:null, motDePasse:null, statut:null, adresseP:null,  photo:null};
+
+  /* bricolage d'André */
+  adresse: any;
+
+  //prop : pour la gestion de l'aperçu de l'image
+  previewPhoto : boolean = false;
+
+  constructor(private router : Router, 
+              private personneService : PersonneService, 
+              private adressePersonneService : AdressePersonneService,
+              private activatedRoute : ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(
+      (parameterMap) => {
+        //récup du param id de l'url (réf : route personneEdit/:id de app-routing.module.ts)
+        const id = +parameterMap.get("id");
+
+        //appel de la méthode findPersonneById() avec la valeur du param pour la récup du client à modifier
+        this.findPersonneById(id);
+      }
+    );
   }
+
+  //méthode pour la gestion de l'aperçu de la photo
+  togglePhotoPreview(){
+    this.previewPhoto = !this.previewPhoto;
+  }
+
+  /**
+   * > sera invoquée à la soumission du formulaire de create-personne.component.html
+   *    -> lors de l'ajout.
+   *    -> lors de la modification du personne
+   * > appel le service PersonneService pour l'ajout du personne
+   */
+  saveOrUpdatePersonne(){
+
+    //--> test du personne à ajouter ou à modifier
+    if(this.personne.identifiant == null){
+      //--------- nouveau personne -----------//
+      console.log("1-ID de l'adresse : "+ this.adresse);
+      this.adressePersonneService.findAdressePersonneById(this.adresse).subscribe(
+        (adresse) => { this.personne.adresseP = adresse; console.log("1-ID de l'adressePersonne : "+ this.personne.adresseP.rue);}
+      );
+
+      //-> appel du service PersonneService pour l'ajout de l'personne
+      this.personneService.ajouterPersonne(this.personne).subscribe(
+        (data) => { console.log(data); }
+      );
+
+    }else{
+      //--------- modification d'un personne -----------//
+      //-> invocaion de la méthode du service modifierPersonne() pour la maj de l'personne
+      this.personneService.modifierPersonne(this.personne).subscribe(
+        () => {  }
+      );
+
+    }
+
+    //=> redirection vers la liste des personnes via la route '/list'
+    this.router.navigate(['personneList']);
+    console.log(this.personne);
+  }
+
+
+
+  /**
+   * permet de :
+   *    -> cas id=0 : initialiser un objet vide pour l'ajout
+   *    -> cas id !=0 : appel du service pour la récup de l'personne pour modif
+   * @param idPersonne
+   */
+  findPersonneById(idPersonne : number){
+    if(idPersonne == 0){
+      //--------- ajout ----------//
+      this.personne = {identifiant:null, nom:null, email:null, motDePasse:null, statut:null, adresseP:null,  photo:null};
+
+    }else{
+      //-------- modif ---------//
+      this.personneService.findPersonneById(idPersonne).subscribe(
+        (personneModif) => {this.personne = personneModif}
+      );
+    }
+  }
+
+  listPersonne() {
+    this.router.navigate(['personneList']);
+  } //end editPersonne
+
+/*   email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  hide = true; */
 
 }
--- angularTPImmo/src/app/composants/Personnes/personne-composants/liste-personnes/liste-personnes.component.ts
+++ angularTPImmo/src/app/composants/Personnes/personne-composants/liste-personnes/liste-personnes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonneService } from '../../../../services/personne.service';
+import { IPersonne } from '../../../../modele/IPersonne';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-liste-personnes',
@@ -7,9 +10,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListePersonnesComponent implements OnInit {
 
-  constructor() { }
+  //prop : tableau d'personnes
+  personnes = [];
+
+  //prop : pour la gestion de l'aperçu des infos supplémentaires
+  previewInfos : boolean = false;
+
+  constructor(private personneService : PersonneService, private router : Router) { }
 
   ngOnInit(): void {
+    //rafraichissement de la page
+    this.personneService.refreshNeeded.subscribe(
+      () => { this.getAllPersonnes(); }
+    );
+    this.getAllPersonnes();
   }
+
+  /**
+   * récup de la liste des personnes via le service
+   */
+  getAllPersonnes(){
+
+    //abonnement au service
+    this.personneService.getAllPersonne().subscribe(
+      data => this.personnes = data
+    );
+  }
+
+
+  /**
+   * permet de supprimer un personne via le service PersonneService
+   * @param personne 
+   */
+  deletePersonne(personne : IPersonne){
+
+    //abonnement au service PersonneService
+    this.personneService.supprimerPersonne(personne).subscribe(
+      () => { this.personnes.filter(cl => cl != personne); }
+    );
+
+  }
+
+
+
+  /**
+   * permet de naviguer vers 'edit/id' en ajoutant
+   * l'id de l'personne à cette route
+   * @param idAdministrateur
+   */
+  editAdministrateur(idAdministrateur: number) {
+    this.router.navigate(['administrateurEdit/', idAdministrateur]);
+  } //end editAdministrateur
+
+  editAgent(idAgent: number) {
+    this.router.navigate(['agentEdit/', idAgent]);
+  } //end editAgent
+
+  editClient(idClient: number) {
+    this.router.navigate(['clientEdit/', idClient]);
+  } //end editClient
+
+  editProprietaire(idProprietaire: number) {
+    this.router.navigate(['proprietaireEdit/', idProprietaire]);
+  } //end editProprietaire
+
+  //méthode pour la gestion de l'aperçu des infos
+  toggleInfosPreview(){
+    this.previewInfos = !this.previewInfos;
+  }
+
+  //méthode pour la pop-up
+  /* openModal(liste_visites : IPersonne["liste_visites"]){
+    document.getElementById("modal").style.top = "0px";
+  } */
 
 }
